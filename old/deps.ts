3468f4174af7e65be45ebee66c25da1d4bc60a56
Cameron Silvestrini <csilvestrini@google.com>
2025-06-04 10:09:24 +1000

    Deletes old/

--- old/deps.ts
+++
@@ -1,7 +0,0 @@
-export * as path from "https://deno.land/std@0.126.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.126.0/fs/mod.ts";
-export {
-  assert,
-  assertEquals,
-  assertThrows,
-} from "https://deno.land/std@0.126.0/testing/asserts.ts";
--- old/examples/nested/tools/append.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script to simulate running a compiler.
-# Copies the input file and appends a bit to the end.
-IN="$1"
-OUT="$2"
-cp "$IN" "$OUT"
-echo xyz >> "$OUT"
